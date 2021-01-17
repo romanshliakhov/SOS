69a0b2f90386fdf716735bdf752a2623595884c9
romanshliakhov <romanshliakhov@gmail.com>
2021-01-17 17:59:42 +0200

    added animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,18 @@
 $(function() {
-
 	$('select').styler();
-
 });
+
+var text = $('.menu').text(),
+textArr = a.split('');
+
+$('.menu').html('');
+
+$.each(textArr, function(i, v){
+if(v == ' '){$('.menu').append('<a class="space"></a>');}
+$('.menu').append('<a>'+v+'</a>');
+})
+
+
+
+
+
