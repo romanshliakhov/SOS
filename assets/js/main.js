44605353a6fc759dcad2fe6dd20fdc627c1d0deb
romanshliakhov <romanshliakhov@gmail.com>
2021-01-19 18:18:32 +0200

    added tabs

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,13 @@ $(function() {
 	$('select').styler();
 });
 
+$(document).ready(function(){
+	$('.features-tab_title').click(function(){
+		$(this).next('.features-tab_descr').slideToggle();
+	})
+});
+
+
 var text = $('.menu').text(),
 textArr = a.split('');
 
@@ -10,7 +17,8 @@ $('.menu').html('');
 $.each(textArr, function(i, v){
 if(v == ' '){$('.menu').append('<a class="space"></a>');}
 $('.menu').append('<a>'+v+'</a>');
-})
+});
+
 
 
 
